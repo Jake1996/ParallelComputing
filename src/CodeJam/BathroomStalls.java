a5d5a29012998a8b55df41109a60b470f7a48d8a
Jake1996 <jatinpm96@gmail.com>
2017-04-16 19:53:06 +0530

    batroom stalls parallelized

--- src/CodeJam/BathroomStalls.java
+++ src/CodeJam/BathroomStalls.java
@@ -1,5 +1,8 @@
 package CodeJam;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * https://code.google.com/codejam/contest/3264486/dashboard#s=p2
  * @author jake
@@ -8,12 +11,37 @@ import java.util.Scanner;
 public class BathroomStalls {
 
 	public static void main(String[] args) {
+		ExecutorService service = Executors.newFixedThreadPool(5);
 		Scanner in = new Scanner(System.in);
 		int tc = in.nextInt();
 		int temp = tc;
+		Task result[] = new Task[tc];
 		while(tc--!=0) {
 			long n = in.nextLong();
 			long k = in.nextLong();
+			result[temp-tc-1] = new Task(temp-tc, n,k);
+			service.submit(result[temp-tc-1]);
+		}
+		in.close();
+		service.shutdown();
+		while(!service.isTerminated()) {}
+		for (int i =0; i<temp; i++){
+			System.out.println(result[i].output);
+		}
+
+
+	}
+	static public class Task implements Runnable {
+		private int taskId;
+		long n,k;
+		public String output;
+		public Task(int i,long n,long k) {
+			this.taskId = i;
+			this.n = n;
+			this.k = k;
+		}
+		@Override
+		public void run() {
 			n-= (k-largestPower(k));
 			k = largestPower(k);
 			n /= k; 
@@ -24,11 +52,11 @@ public class BathroomStalls {
 				add = 1;
 			}
 			l = r = n>>1;
-			System.out.println("Case #"+(temp-tc)+": "+(l+add)+" "+r);
-		}
-		in.close();
 
-	}
+					output = "Case #"+(taskId)+": "+(l+add)+" "+r;
+			}
+
+		}
 	public static long largestPower(long n){
 		int count = 0;
 		while(n!=1) {
@@ -36,10 +64,10 @@ public class BathroomStalls {
 			count++;
 		}
 		return (long)Math.pow(2, count);
-//		n = n|(n>>1);
-//		n = n|(n>>2);
-//		n = n|(n>>4);
-//		n = n|(n>>8);
-//		return (n+1)>>1;
+		//		n = n|(n>>1);
+		//		n = n|(n>>2);
+		//		n = n|(n>>4);
+		//		n = n|(n>>8);
+		//		return (n+1)>>1;
 	}
 }
